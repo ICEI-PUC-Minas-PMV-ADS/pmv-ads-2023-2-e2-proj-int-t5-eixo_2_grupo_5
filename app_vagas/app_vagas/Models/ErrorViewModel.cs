4ef580f0f7892d8a092012c0b59499824958be22
Eddy Moura <mouragestao@gmail.com>
2023-10-07 08:31:27 -0300

    Initial commit

---
+++ app_vagas/app_vagas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace app_vagas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
