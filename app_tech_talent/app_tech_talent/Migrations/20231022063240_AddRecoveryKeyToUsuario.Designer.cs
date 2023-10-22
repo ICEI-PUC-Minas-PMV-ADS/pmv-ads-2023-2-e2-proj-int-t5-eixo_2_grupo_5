8dc9af06651a1b943c6dbab4d6decc439eb6c3b9
Daniel Drumond <daniel.drumond@begrowth.com.br>
2023-10-22 05:48:40 -0300

    feat: add migration

--- app_tech_talent/app_tech_talent/Migrations/20231022063240_AddRecoveryKeyToUsuario.Designer.cs
+++ app_tech_talent/app_tech_talent/Migrations/20231022063240_AddRecoveryKeyToUsuario.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using app_tech_talent.Models;
 
@@ -11,9 +12,10 @@ using app_tech_talent.Models;
 namespace app_tech_talent.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231022063240_AddRecoveryKeyToUsuario")]
+    partial class AddRecoveryKeyToUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ app_tech_talent/app_tech_talent/Migrations/20231022063240_AddRecoveryKeyToUsuario.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace app_tech_talent.Migrations
+{
+    public partial class AddRecoveryKeyToUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RecoveryKey",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecoveryKey",
+                table: "Usuarios");
+        }
+    }
+}
