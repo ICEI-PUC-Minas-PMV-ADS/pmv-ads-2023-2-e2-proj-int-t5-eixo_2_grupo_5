0454af4bcdf893bd3da77eacdc8a1019c23c62a0
lafayete <lafayeteqhorta@gmail.com>
2023-11-09 10:58:10 -0300

    Vagas-NaoConcluidas

---
+++ app_tech_talent/app_tech_talent/Controllers/VagasController.cs
@@ -0,0 +1,24 @@
+﻿using app_tech_talent.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace app_tech_talent.Controllers
+{
+    [Authorize]
+    public class VagasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VagasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Vagas.ToListAsync();
+
+            return View(dados);
+        }
+    }
+}
