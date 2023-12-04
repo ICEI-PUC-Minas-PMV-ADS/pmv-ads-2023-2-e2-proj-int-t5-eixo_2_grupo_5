cb033d84a676818252a792453c621b095456b0ca
lelevila <lele.ran.vile@gmail.com>
2023-12-04 18:00:11 -0300

    Corrigindo formatação de formação acadêmica

--- app_tech_talent/app_tech_talent/Models/FormacaoAcademica.cs
+++ app_tech_talent/app_tech_talent/Models/FormacaoAcademica.cs
@@ -20,7 +20,8 @@ namespace app_tech_talent.Models
         [Display(Name = "Grau obtido")]
         public string grauObtido { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy}", ApplyFormatInEditMode = true)]
         [Display(Name = "Ano de conclusão")]
         public DateTime AnoDeConclusao { get; set; }
 
